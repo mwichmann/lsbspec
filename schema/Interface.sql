8d73ce32ae16782ee49307e53d69b577d8cbcc66
anderson <>
2000-09-16 20:14:40 +0000

    [project @ 2000-09-16 20:14:24 by anderson] Add Architecture relationships

--- schema/Interface.sql
+++ schema/Interface.sql
@@ -8,6 +8,7 @@ CREATE TABLE Interface (
   Istatus enum('Included','Excluded','Unknown') DEFAULT 'Unknown' NOT NULL,
   Itype enum('Function','Data','Unknown') DEFAULT 'Unknown' NOT NULL,
   Istandard int(10) DEFAULT '0' NOT NULL,
+  Iarch int(10) DEFAULT '0' NOT NULL,
   Iheader int(10) DEFAULT '0' NOT NULL,
   Ilibgroup int(10) DEFAULT '0' NOT NULL,
   Ireturn int(11) DEFAULT '0' NOT NULL,
--- schema/LibGroup.sql
+++ schema/LibGroup.sql
@@ -6,6 +6,7 @@ CREATE TABLE LibGroup (
   LGid int(10) DEFAULT '0' NOT NULL auto_increment,
   LGname varchar(60) DEFAULT '' NOT NULL,
   LGlib int(10) DEFAULT '0' NOT NULL,
+  LGarch int(10) DEFAULT '0' NOT NULL,
   LGorder int(10),
   LGdescription text NOT NULL,
   PRIMARY KEY (LGid)
