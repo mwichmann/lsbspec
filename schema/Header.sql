c14fb8f39d7acc65e877730e9be59184ad48f903
anderson <>
2000-09-17 05:58:32 +0000

    [project @ 2000-09-17 05:58:32 by anderson] Fix the name of the table

--- schema/Header.sql
+++ schema/Header.sql
@@ -1,8 +1,8 @@
 #
-# Table structure for table 'Headers'
+# Table structure for table 'Header'
 #
-DROP TABLE IF EXISTS Headers;
-CREATE TABLE Headers (
+DROP TABLE IF EXISTS Header;
+CREATE TABLE Header (
   Hid int(10) DEFAULT '0' NOT NULL auto_increment,
   Hname varchar(60) DEFAULT '' NOT NULL,
   PRIMARY KEY (Hid)
