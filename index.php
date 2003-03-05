61e2f0e15d7c9d6ddfd405d271ac6a6a699363cf
anderson <>
2003-03-05 21:41:10 +0000

    [project @ 2003-03-05 21:41:10 by anderson] Redo the index to be in php so the include an flastmod fucntionallity is restored

---
+++ index.php
@@ -0,0 +1,328 @@
+<!doctype html public "-//w3c//dtd html 4.0 transitional//en">
+<html>
+<head>
+   <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+   <title>LSB Written Specification</title>
+   <LINK REL="StyleSheet" HREF="/themes/LSB/style/style.css" TYPE="text/css">
+</head>
+<body>
+
+<?
+virtual("/themes/LSB/header.inc");
+?>
+<hr>
+<P>
+The LSB specification is composed of a set of specifications:
+a single common specification, and architecture specific specifications. The
+complete specification for a platform consists of the common specification plus
+one of the architecture specifications.
+
+<H1>The LSB 1.3 is Complete!</H1>
+The 1.3 version of the LSB is now complete.
+
+<H1>LSB 1.3 Specification</H1>
+The 1.3 release consists of the common specification, and a processor specific
+specification for the IA32 and PPC32 architectures. These documents should be
+used together to obtain the complete specification.
+<TABLE>
+<TR>
+<TH>Specification</TH>
+<TH>Version</TH>
+<TH>HTML</TH>
+<TH>RTF</TH>
+<TH>PDF</TH>
+<TH>One Page HTML</TH>
+<TH>TXT</TH>
+<TH>TXT (numbered)</TH>
+</TR>
+<TR>
+<TD>LSB Common</TD>
+<TD ALIGN=center><B>
+<?
+include("refspecs/LSB_1.3.0/lsbversion")
+?>
+</B></TD>
+<TD><a href="refspecs/LSB_1.3.0/gLSB/gLSB/book1.html">HTML</a></TD>
+<TD><a href="refspecs/LSB_1.3.0/gLSB/gLSB.rtf">RTF</a></TD>
+<TD><a href="refspecs/LSB_1.3.0/gLSB/gLSB.pdf">PDF</a></TD>
+<TD ALIGN=center><a href="refspecs/LSB_1.3.0/gLSB/gLSB.html">HTML</a></TD>
+<TD ALIGN=center><a href="refspecs/LSB_1.3.0/gLSB/gLSB.txt">TXT</a></TD>
+<TD ALIGN=center><a href="refspecs/LSB_1.3.0/gLSB/gLSB_lines.txt">TXT</a></TD>
+</TR>
+<TR>
+<TD>LSB IA32</TD>
+<TD ALIGN=center><B>
+<?
+include("refspecs/LSB_1.3.0/IA32/lsbversion.IA32")
+?>
+</B></TD>
+<TD><a href="refspecs/LSB_1.3.0/IA32/spec/book1.html">HTML</a></TD>
+<TD><a href="refspecs/LSB_1.3.0/IA32/spec.rtf">RTF</a></TD>
+<TD><a href="refspecs/LSB_1.3.0/IA32/spec.pdf">PDF</a></TD>
+<TD ALIGN=center><a href="refspecs/LSB_1.3.0/IA32/spec.html">HTML</a></TD>
+<TD ALIGN=center><a href="refspecs/LSB_1.3.0/IA32/spec.txt">TXT</a></TD>
+<TD ALIGN=center><a href="refspecs/LSB_1.3.0/IA32/spec_lines.txt">TXT</a></TD>
+</TR>
+<TR>
+<TD>LSB IA64</TD>
+<TD ALIGN=center><B>
+<?
+include("refspecs/LSB_1.3.0/IA64/lsbversion.IA64")
+?>
+</B></TD>
+<TD><a href="refspecs/LSB_1.3.0/IA64/spec/book1.html">HTML</a></TD>
+<TD><a href="refspecs/LSB_1.3.0/IA64/spec.rtf">RTF</a></TD>
+<TD><a href="refspecs/LSB_1.3.0/IA64/spec.pdf">PDF</a></TD>
+<TD ALIGN=center><a href="refspecs/LSB_1.3.0/IA64/spec.html">HTML</a></TD>
+<TD ALIGN=center><a href="refspecs/LSB_1.3.0/IA64/spec.txt">TXT</a></TD>
+<TD ALIGN=center><a href="refspecs/LSB_1.3.0/IA64/spec_lines.txt">TXT</a></TD>
+</TR>
+<TR>
+<TD>LSB PPC32</TD>
+<TD ALIGN=center><B>
+<?
+include("refspecs/LSB_1.3.0/PPC32/lsbversion.PPC32")
+?>
+</B></TD>
+<TD><a href="refspecs/LSB_1.3.0/PPC32/spec/book1.html">HTML</a></TD>
+<TD><a href="refspecs/LSB_1.3.0/PPC32/spec.rtf">RTF</a></TD>
+<TD><a href="refspecs/LSB_1.3.0/PPC32/spec.pdf">PDF</a></TD>
+<TD ALIGN=center><a href="refspecs/LSB_1.3.0/PPC32/spec.html">HTML</a></TD>
+<TD ALIGN=center><a href="refspecs/LSB_1.3.0/PPC32/spec.txt">TXT</a></TD>
+<TD ALIGN=center><a href="refspecs/LSB_1.3.0/PPC32/spec_lines.txt">TXT</a></TD>
+</TR>
+<TR>
+<TD>LSB Errata</TD>
+<TD ALIGN=center><B>1.3</B></TD>
+<TD><a href="errata/errata/book1.html">HTML</a></TD>
+<TD ALIGN=center></TD>
+<TD ALIGN=center></TD>
+<TD ALIGN=center></TD>
+<TD ALIGN=center></TD>
+<TD ALIGN=center></TD>
+</TR>
+</TABLE>
+
+<H2><A NAME="DRAFT">The Current Draft</A></H2>
+The current draft of the LSB Specification is available both online and as
+a RTF file.  
+We welcome and appreciate comments and bug reports at any time (see
+"Review" links below).  You don't need to wait for a public review period.
+<TABLE>
+<TR>
+<TH>Specification</TH>
+<TH>Version</TH>
+<TH>Review Form</TH>
+<TH>HTML</TH>
+<TH>RTF</TH>
+<TH>PDF</TH>
+<TH>One Page HTML</TH>
+<TH>TXT</TH>
+<TH>TXT (numbered)</TH>
+<TH>Last Modified on ..</TH>
+</TR>
+<TR>
+<TD>LSB Common</TD>
+<TD ALIGN=center><B>
+<?
+include("lsbversion")
+?>
+</B></TD>
+<TD ALIGN=center><A HREF="lsbreview.html">Review it!</A></TD>
+<TD ALIGN=center><a href="gLSB/gLSB/book1.html">HTML</a></TD>
+<TD ALIGN=center><a href="gLSB/gLSB.rtf">RTF</a></TD>
+<TD ALIGN=center><a href="gLSB/gLSB.pdf">PDF</a></TD>
+<TD ALIGN=center><a href="gLSB/gLSB.html">HTML</a></TD>
+<TD ALIGN=center><a href="gLSB/gLSB.txt">TXT</a></TD>
+<TD ALIGN=center><a href="gLSB/gLSB_lines.txt">TXT</a></TD>
+<TD>
+<?
+echo(date("r", filemtime("gLSB/gLSB/book1.html")))
+?>
+</TD>
+</TR>
+<TR>
+<TD>LSB IA32</TD>
+<TD ALIGN=center><B>
+<?
+include("lsbversion.IA32")
+?>
+</B></TD>
+<TD ALIGN=center><A HREF="lsbia32review.html">Review it!</A></TD>
+<TD ALIGN=center><a href="archLSB/IA32/spec/book1.html">HTML</a></TD>
+<TD ALIGN=center><a href="archLSB/IA32/spec.rtf">RTF</a></TD>
+<TD ALIGN=center><a href="archLSB/IA32/spec.pdf">PDF</a></TD>
+<TD ALIGN=center><a href="archLSB/IA32/spec.html">HTML</a></TD>
+<TD ALIGN=center><a href="archLSB/IA32/spec.txt">TXT</a></TD>
+<TD ALIGN=center><a href="archLSB/IA32/spec_lines.txt">TXT</a></TD>
+<TD>
+<?
+echo(date("r", filemtime("archLSB/IA32/spec/book1.html")))
+?>
+</TD>
+</TR>
+<TR>
+<TD>LSB IA64</TD>
+<TD ALIGN=center><B>
+<?
+include("lsbversion.IA64")
+?>
+</B></TD>
+<TD ALIGN=center><A HREF="lsbia64review.html">Review it!</A></TD>
+<TD ALIGN=center><a href="archLSB/IA64/spec/book1.html">HTML</a></TD>
+<TD ALIGN=center><a href="archLSB/IA64/spec.rtf">RTF</a></TD>
+<TD ALIGN=center><a href="archLSB/IA64/spec.pdf">PDF</a></TD>
+<TD ALIGN=center><a href="archLSB/IA64/spec.html">HTML</a></TD>
+<TD ALIGN=center><a href="archLSB/IA64/spec.txt">TXT</a></TD>
+<TD ALIGN=center><a href="archLSB/IA64/spec_lines.txt">TXT</a></TD>
+<TD>
+<?
+echo(date("r", filemtime("archLSB/IA64/spec/book1.html")))
+?>
+</TD>
+</TR>
+<TR>
+<TD>LSB PPC32</TD>
+<TD ALIGN=center><B>
+<?
+include("lsbversion.PPC32")
+?>
+</B></TD>
+<TD ALIGN=center><A HREF="lsbppc32review.html">Review it!</A></TD>
+<TD ALIGN=center><a href="archLSB/PPC32/spec/book1.html">HTML</a></TD>
+<TD ALIGN=center><a href="archLSB/PPC32/spec.rtf">RTF</a></TD>
+<TD ALIGN=center><a href="archLSB/PPC32/spec.pdf">PDF</a></TD>
+<TD ALIGN=center><a href="archLSB/PPC32/spec.html">HTML</a></TD>
+<TD ALIGN=center><a href="archLSB/PPC32/spec.txt">TXT</a></TD>
+<TD ALIGN=center><a href="archLSB/PPC32/spec_lines.txt">TXT</a></TD>
+<TD>
+<?
+echo(date("r", filemtime("archLSB/PPC32/spec/book1.html")))
+?>
+</TD>
+</TR>
+<TR>
+<TD>LSB PPC64</TD>
+<TD ALIGN=center><B>
+<?
+include("lsbversion.PPC64")
+?>
+</B></TD>
+<TD ALIGN=center><A HREF="lsbppc64review.html">Review it!</A></TD>
+<TD ALIGN=center><a href="archLSB/PPC64/spec/book1.html">HTML</a></TD>
+<TD ALIGN=center><a href="archLSB/PPC64/spec.rtf">RTF</a></TD>
+<TD ALIGN=center><a href="archLSB/PPC64/spec.pdf">PDF</a></TD>
+<TD ALIGN=center><a href="archLSB/PPC64/spec.html">HTML</a></TD>
+<TD ALIGN=center><a href="archLSB/PPC64/spec.txt">TXT</a></TD>
+<TD ALIGN=center><a href="archLSB/PPC64/spec_lines.txt">TXT</a></TD>
+<TD>
+<?
+echo(date("r", filemtime("archLSB/PPC64/spec/book1.html")))
+?>
+</TD>
+</TR>
+<TR>
+<TD>LSB S390</TD>
+<TD ALIGN=center><B>
+<?
+include("lsbversion.S390")
+?>
+</B></TD>
+<TD ALIGN=center><A HREF="lsbs390review.html">Review it!</A></TD>
+<TD ALIGN=center><a href="archLSB/S390/spec/book1.html">HTML</a></TD>
+<TD ALIGN=center><a href="archLSB/S390/spec.rtf">RTF</a></TD>
+<TD ALIGN=center><a href="archLSB/S390/spec.pdf">PDF</a></TD>
+<TD ALIGN=center><a href="archLSB/S390/spec.html">HTML</a></TD>
+<TD ALIGN=center><a href="archLSB/S390/spec.txt">TXT</a></TD>
+<TD ALIGN=center><a href="archLSB/S390/spec_lines.txt">TXT</a></TD>
+<TD>
+<?
+echo(date("r", filemtime("archLSB/S390/spec/book1.html")))
+?>
+</TD>
+</TR>
+<TR>
+<TD>LSB S390X</TD>
+<TD ALIGN=center><B>
+<?
+include("lsbversion.S390X")
+?>
+</B></TD>
+<TD ALIGN=center><A HREF="lsbs390xreview.html">Review it!</A></TD>
+<TD ALIGN=center><a href="archLSB/S390X/spec/book1.html">HTML</a></TD>
+<TD ALIGN=center><a href="archLSB/S390X/spec.rtf">RTF</a></TD>
+<TD ALIGN=center><a href="archLSB/S390X/spec.pdf">PDF</a></TD>
+<TD ALIGN=center><a href="archLSB/S390X/spec.html">HTML</a></TD>
+<TD ALIGN=center><a href="archLSB/S390X/spec.txt">TXT</a></TD>
+<TD ALIGN=center><a href="archLSB/S390X/spec_lines.txt">TXT</a></TD>
+<TD>
+<?
+echo(date("r", filemtime("archLSB/S390X/spec/book1.html")))
+?>
+</TD>
+</TR>
+</TABLE>
+Additional formats and architectures will become available in the future.
+
+<H2><A NAME="WIP">Works In Progess</A></H2>
+The LSB Futures group has a number of proposals in various stages of readiness.
+This document provide a place for these <I>works-in-progress</I> to live while
+they are being developed. Once a feature is ready, it then gets moved into the
+main specification document.
+<TABLE>
+<TR>
+<TH>Specification</TH>
+<TH>HTML</TH>
+<TH>RTF</TH>
+<TH>PDF</TH>
+<TH>One Page HTML</TH>
+<TH>TXT</TH>
+<TH>TXT (numbered)</TH>
+<TH>Last Modified on ..</TH>
+</TR>
+<TR>
+<TD>LSB WIP</TD>
+<TD><a href="wip/wip/book1.html">HTML</a></TD>
+<TD><a href="wip/wip.rtf">RTF</a></TD>
+<TD></TD>
+<TD ALIGN=center><a href="wip/wip.html">HTML</a></TD>
+<TD ALIGN=center><a href="wip/wip.txt">TXT</a></TD>
+<TD ALIGN=center><a href="wip/wip.txt">TXT</a></TD>
+<TD>
+<?
+echo(date("r", filemtime("wip/wip/book1.html")))
+?>
+</TD>
+</TR>
+</TABLE>
+
+<H2>Older version</H2>
+<P>
+Past version of the specification can be found <A HREF=oldspecs.shtml>Here</A>.
+
+<H2>About the Specification</H2>
+The LSB Specification is written in OASIS V4.1 Docbook SGML.&nbsp; You
+can&nbsp; <a href="http://sourceforge.net/cvs/?group_id=1107">checkout
+a copy</a> of the Docbook source from the LSB project on SourceForge.Net.
+Portions of the document are produced from a <a href="db.shtml">database</a>
+that was designed and is maintained by the LSB. In the spec tree there
+is a <i>build.html </i>file which describes <a href="build.html">how to
+build the specification</a> from the CVS Docbook source.
+<H2><a NAME="contribute"></a>How to Contribute</H2>
+The LSB Specification is a community effort.&nbsp; There are several ways
+for you to contribute.&nbsp;&nbsp; The first way for you to contribute
+is to read and review the specification, then email your questions and
+comments to the <i>lsb-spec</i> <a href="../lists.html">email list</a>.&nbsp;&nbsp;
+The second way is to send your corrections for the spec in context diff
+format to the <i>lsb-spec</i> email list.&nbsp;&nbsp; The third way is
+to <a href="http://sourceforge.net/project/memberlist.php?group_id=1107">find
+an LSB developer</a>, then send him/her your corrections.&nbsp; The final
+way to contribute to the LSB written specification is to become an LSB
+developer to make CVS changes directly to the tree with your and other's
+submitted changes. Here is a list of <a href="http://sourceforge.net/pm/task.php?group_id=1107&set=custom&group_project_id=3049&_assigned_to=100&_status=1&SUBMIT=Browse">things
+to do</a> for the LSB Specification.
+<H2><a NAME="faq"></a>Frequently Asked Questions</H2>
+An FAQ associated with the written specification is available <a href="faq.html">here</a>.
+<hr WIDTH="100%">
+</body>
+</html>
