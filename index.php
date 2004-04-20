0a66b7d12179939c0b94c6ff17fa6e873c8d0404
anderson <>
2004-04-20 22:56:18 +0000

    [project @ 2004-04-20 22:56:18 by anderson] Reduce what is on this page, some of which is redundant with the stuff in teh header

--- index.php
+++ index.php
@@ -63,11 +63,11 @@ These will become LSB 2.0 upon release.</p>
 <p>
 The 1.3 version of the LSB can be found <A HREF=oldspecs.shtml>Here</A>.</p>
 
-<H1>Older versions</H1>
+<H2>Older versions</H2>
 <p>
 Previous versions of the specification can be found <A HREF=oldspecs.shtml>Here</A>.</p>
 
-<H1>About the Specification</H1>
+<H3>About the Specification</H3>
 <p>
 The LSB Specification is written in OASIS V4.1 Docbook SGML.&nbsp; You
 can&nbsp; <a href="http://gforge.freestandards.org/scm/?group_id=5">checkout
@@ -76,26 +76,6 @@ Portions of the document are produced from a <a href="db.shtml">database</a>
 that was designed and is maintained by the LSB. In the spec tree there
 is a <i>build.html </i>file which describes <a href="build.html">how to
 build the specification</a> from the CVS Docbook source.</p>
-
-<H1><a NAME="contribute"></a>How to Contribute</H1>
-<p>
-The LSB Specification is a community effort.&nbsp; There are several ways
-for you to contribute.&nbsp;&nbsp; The first way for you to contribute
-is to read and review the specification, then email your questions and
-comments to the <i>lsb-spec</i> <a href="../lists.html">email list</a>.&nbsp;&nbsp;
-The second way is to send your corrections for the spec in context diff
-format to the <i>lsb-spec</i> email list.&nbsp;&nbsp; The third way is
-to <a href="http://gforge.freestandards.org.net/project/memberlist.php?group_id=5">find
-an LSB developer</a>, then send him/her your corrections.&nbsp; The final
-way to contribute to the LSB written specification is to become an LSB
-developer to make CVS changes directly to the tree with your and other's
-submitted changes. Here is a list of <a href="http://sourceforge.net/pm/task.php?group_id=1107&amp;set=custom&amp;group_project_id=3049&amp;_assigned_to=100&amp;_status=1&amp;SUBMIT=Browse">things
-to do</a> for the LSB Specification.</p>
-
-<H1><a NAME="faq"></a>Frequently Asked Questions</H1>
-<p>
-An FAQ associated with the written specification is available
-<a href="http://www.linuxbase.org/modules.php?name=FAQ&amp;myfaq=yes&amp;id_cat=3&amp;categories=LSB+Written+Specification">here</a>.</p>
 
 </td></tr>
 </table>
